6371338963e988b2912680dd9228a820ec994adf
Dharshinipa <126184056+Dharshinipa@users.noreply.github.com>
2023-08-24 22:43:19 +0530

    Add files via upload

---
+++ array5.java
@@ -0,0 +1,10 @@
+public class PrintArrayElements {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5};
+
+        System.out.println("Array elements:");
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+    }
+}
